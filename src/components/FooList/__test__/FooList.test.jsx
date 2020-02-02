f325176c38dfb8785021f862c5fa17a62517244d
rkaland <ruudupwork@gmail.com>
2020-02-02 21:11:38 +0200

    Add a test that check if FooList renders two foos

--- src/components/FooList/__test__/FooList.test.jsx
+++ src/components/FooList/__test__/FooList.test.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import FooList from '../';
 
 describe('FooList', () => {
+  const renderFooList = props => shallow(<FooList {...props} />);
+
   it('renders correctly', () => {
-    const wrapper = shallow(<FooList />);
+    const wrapper = renderFooList({});
     expect(wrapper.find('div.container')).toHaveLength(1);
+  });
+
+  it('renders two foos', () => {
+    const dumpFoos = [
+      {
+        id: 1,
+        colour: '1',
+        speed: '1',
+        size: '1',
+      },
+      {
+        id: 2,
+        colour: '2',
+        speed: '2',
+        size: '2',
+      },
+    ];
+    const wrapper = renderFooList({ fooList: dumpFoos });
+
+    expect(wrapper.find('li.list-group-item')).toHaveLength(2);
   });
 });
