5cfd4e949313a06b47316028b742984ef2ecdf25
rkaland <ruudupwork@gmail.com>
2020-02-02 20:44:28 +0200

    Add Enzyme package Prettify the code base Add App.test.jsx for testing app.js

---
+++ src/containers/FooList/__test__/FooList.test.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+
+import FooList from '../';
+
+describe('FooList', () => {
+  it('Should render correctly', () => {});
+});
